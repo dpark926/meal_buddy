511ef238d8cb949a3c9180c29ac7a02be1e76e73
Daniel Park <dpeace926@gmail.com>
2018-11-05 20:11:48 -0500

    start over client, next init

--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9f870fe9abefdfb82eff";
+/******/ 	var hotCurrentHash = "cdeca3d1e050e1f7fe12";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
