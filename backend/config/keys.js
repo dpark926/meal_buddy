6e7cca7905f110aab3c7b11b4f6107f089d6aeb7
Daniel Park <dpeace926@gmail.com>
2018-12-04 21:42:47 -0500

    create jwt

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://dpark926:learnc0de@ds225624.mlab.com:25624/meal_buddy"
+  mongoURI: "mongodb://dpark926:learnc0de@ds225624.mlab.com:25624/meal_buddy",
+  secretOrKey: "secret"
 };
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 //Load User model
 const User = require("../../models/User");
@@ -55,7 +57,23 @@ router.post("/login", (req, res) => {
     // Check Password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "Success" });
+        // res.json({ msg: "Success" });
+
+        // User matched
+        const payload = { id: user.id, name: user.name }; // Create JWT payload
+
+        // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password incorrect" });
       }
